e02dc43dcf8e589c158822e8b560df823fde2318
Dave Cho <kaiser0906@hotmail.com>
2016-03-01 14:04:39 -0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 from os.path import dirname
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db import connections
 from invoke import run, task
+from django.db.utils import OperationalError
 
 
 base = dirname(__file__)
@@ -9,23 +11,43 @@ DEFAULT_DB_NAME = settings.DATABASES['default']['NAME']
 DEFAULT_USERNAME = 'dcho'
 DEFAULT_EMAIL = 'dcho@scrollmotion.com'
 DEFAULT_CLIENT_ID = 'GZP5HlyiBwalItizXJbpY6iBoJhVKjmde0FU1bCo'
+DEFAULT_APPS = ['core', 'customer', 'password', 'e_mail', 'content']
 
 
 @task
-def test():
-    run('simplestudio/manage.py test core customer')
+def test(apps=None):
+    '''
+    Run django unittest
+    '''
+    if not apps:
+        apps = ' '.join(DEFAULT_APPS)
+    run('{}/manage.py test {}'.format(base, apps))
 
 
 @task
 def clean(db=False, db_name=DEFAULT_DB_NAME):
+    '''
+    If db is set to True, this process will drop the database if exists and
+    recreate it.
+    '''
     if db:
         print("Database {} is being dropped and recreated.".format(db_name))
-        run("psql -c 'drop database {}'".format(db_name))
+        db_conn = connections['default']
+        try:
+            db_conn.cursor()
+        except OperationalError:
+            pass
+        else:
+            db_conn.close()
+            run("psql -c 'drop database {}'".format(db_name))
         run("psql -c 'create database {}'".format(db_name))
 
 
 @task
 def su(username=None, email=None):
+    '''
+    Create a superuser
+    '''
     email = email or username or DEFAULT_EMAIL
     username = username or DEFAULT_USERNAME
     run("{}/manage.py createsuperuser --username={} --email={}".format(base, username, email).strip())
@@ -34,7 +56,7 @@ def su(username=None, email=None):
 @task
 def build(db=False, db_name=DEFAULT_DB_NAME, admin=False, username=None, email=None):
     clean(db, db_name)
-    run("{}/manage.py makemigrations core customer".format(base))
+    run("{}/manage.py makemigrations {}".format(base, ' '.join(DEFAULT_APPS)))
     run("{}/manage.py migrate".format(base))
     if admin:
         su(username, email)
@@ -44,9 +66,9 @@ def build(db=False, db_name=DEFAULT_DB_NAME, admin=False, username=None, email=N
 
 @task
 def create_application(username=None, email=None):
-    """
+    '''
     This process must require a superuser.
-    """
+    '''
     def get_su():
         try:
             get_user_model().objects.filter(is_superuser=True)[0]
@@ -57,7 +79,11 @@ def create_application(username=None, email=None):
 
 
 @task
-def create_email_templates(file=None, overwrite=False, info=False):
+def import_flatpages(file=None, overwrite=False, info=False):
+    '''
+    Create email templates with using import_flatpages management command
+    that imports a json file to the database.
+    '''
     attrs = ""
     if file:
         attrs += " --file={}".format(file)
@@ -65,11 +91,11 @@ def create_email_templates(file=None, overwrite=False, info=False):
         attrs += " --overwrite=True"
     if info:
         attrs += " --info=True"
-    run("{}/manage.py load_email_templates{}".format(base, attrs))
+    run("{}/manage.py import_flatpages{}".format(base, attrs))
 
 
 @task
 def reset(db=False, db_name=DEFAULT_DB_NAME, username=None, email=None):
     build(db=db, db_name=db_name, admin=True, username=username, email=email)
     create_application()
-    create_email_templates()
+    import_flatpages()
